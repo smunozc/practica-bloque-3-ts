6d7d02459c59b2b9b2e19e4477d5bff1aac04c9d
smunozc <salvador.munoz0109@gmail.com>
2020-11-12 12:52:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# practica-bloque-3-ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/practica-bloque-3-ts)
---
+++ index.ts
@@ -0,0 +1,6 @@
+// En esta práctica debéis crear una interfaz con el nombre "UserData" en el archivo "user-data.ts". En esta interfaz, debéis añadir las propiedades "name", "age" y "phone" con los tipos cadena de texto, número y número respectivamente. Tenéis que exportar esta interfaz.
+// En el documento "index.ts" debéis importar la interfaz que habéis creado en el punto anterior. Cread una variable llamada "user" que sea del tipo de la interfaz que habéis importado.
+// Luego, debéis crear una función llamada "getData" que reciba un tipo genérico, un parámetro del tipo genérico y que saque por consola ese dato.
+// Para terminar, asignad un objeto literal (sin clase) a la variable "user" (con los datos que queráis pero que no dé erroes de tipo) y luego realizad dos llamadas a "getData". La primera llamada debe recibir por parámetros el valor de la propiedad "name" del usuario y la segunda el valor de "phone".
+// OPCIONAL: Dentro de "getData" añadid un condicional con una guarda para diferenciar cuando reciba un tipo "number" y sacar por consola mensajes distintos para las dos llamadas anteriormente mencionadas.
+// Ánimo que ya lo tenéis!
